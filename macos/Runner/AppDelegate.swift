2be4a0801cdcc0959c9b8e64c6564c114149154f
rkishan516 <rkishan516@gmail.com>
2024-07-27 15:58:23 +0530

    Feat: Add status bar for macOS

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,22 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    var statusBar: StatusBarController?
+    var popover = NSPopover.init()
+    
+    override init() {
+        popover.behavior = NSPopover.Behavior.transient
+    }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication)
+    -> Bool {
+        return false
+    }
+    override func applicationDidFinishLaunching(_ aNotification: Notification){
+        let controller: FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
+        popover.contentSize = NSSize(width: 520, height: 400)
+        popover.contentViewController = controller
+        statusBar = StatusBarController.init(popover)
+        mainFlutterWindow?.close()
+        super.applicationDidFinishLaunching(aNotification)
+    }
 }
